657dde59e1d5cecc731b73fe5300255a05c5b923
Jesse <jesseamundsen@gmail.com>
2026-02-24 20:21:14 -0500

    single script for etl

--- data/script_etl.sh
+++ data/script_etl.sh
@@ -8,5 +8,4 @@ curl https://install.duckdb.org | sh
 echo "install spatial; load spatial; install h3 from community; load h3;" >> ~/.duckdbrc
 
 # use duckdb to make (geo)parquet files
-duckdb < data_40.sql
-duckdb < data_50.sql
+duckdb < script_etl.sql
